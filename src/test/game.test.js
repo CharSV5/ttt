a287999e645c5324e385063aef5de769ecd6e459
charlene bastians <charlenebastians@hotmail.com>
2018-09-26 15:28:29 +0100

    checking that swtch player method works the other way round

--- src/test/game.test.js
+++ src/test/game.test.js
@@ -8,4 +8,10 @@ test('switchPlayer switches the player turn', () => {
     const game = new Game('Player X');
     game.switchPlayer('Player X');
     expect(game.showTurn()).toBe('Player O');
-})
+});
+
+test('switchPlayer switches the player turn', () => {
+    const game = new Game('Player O');
+    game.switchPlayer('Player O');
+    expect(game.showTurn()).toBe('Player X');
+});
